aadb4cb537286bd0ca30a3586e59a228e3a24f7b
Callum Curtis <105407637+callumcurtis@users.noreply.github.com>
2025-09-23 17:25:22 -0700

    feat: initial commit

---
+++ data/millbay_corpus/README.txt
@@ -0,0 +1,12 @@
+This corpus was assembled in 2023 by our teaching team from publicly available data.
+
+Data sources:
+jquery-3.6.4.js: JavaScript Library - MIT Licence - Available: https://github.com/jquery/jquery - 294KB
+GCA_009858895.3.fasta: Complete Genome of SARS-CoV-2 - Terms of Use do not specify restrictions on reuse. Available from European Nucleotide Archive: https://www.ebi.ac.uk/ena/browser/view/GCA_009858895.3 - 31KB
+compressed-file-svgrepo-com.svg: SVG image - Public Domain. Available: https://www.svgrepo.com/svg/478376/compressed-file - 2KB
+TheWarOfTheWorlds.txt: txt file - The War of The Worlds by H. G. Wells - Public Domain. Available: https://www.gutenberg.org/ebooks/36
+regional_district_weekly_2021.xlsx: XLSX file - Public Domain. Available: https://catalogue.data.gov.bc.ca/dataset/property-transfer-tax-data-2021
+Historical_Dates.json: JSON file - Historical Dates in a non english language - Public Domain. Available: https://www.vizgr.org/historical-events/search.php?format=json&begin_date=-3000000&end_date=20151231&lang=de 13 MB
+pg1513.epub: ePub File - Romeo and Juliet by William Shakespeare - Public Domain. Available: https://www.gutenberg.org/ebooks/1513 - 147KB
+pear.jpg: JPEG File - Public Domain. Available: https://commons.wikimedia.org/wiki/File:Pear_in_tree_0465.jpg - 160KB
+reboot.c: C Source File - GPL-2.0-only. Available: https://git.launchpad.net/~ubuntu-kernel-test/ubuntu/+source/linux/+git/mainline-crack/tree/kernel/reboot.c
---
+++ flake.nix
@@ -0,0 +1,48 @@
+{
+  description = "gzip";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
+    flake-utils.url = "github:numtide/flake-utils";
+    flamegraph.url = "git+ssh://git@github.com/callumcurtis/snippets?dir=topic/profiling/flamegraph";
+    flamegraph.inputs.nixpkgs.follows = "nixpkgs";
+    flamegraph.inputs.flake-utils.follows = "flake-utils";
+  };
+
+  outputs = { nixpkgs, flake-utils, flamegraph, ... }:
+    flake-utils.lib.eachDefaultSystem (system:
+      let
+        pkgs = nixpkgs.legacyPackages.${system};
+        gzstat = pkgs.stdenv.mkDerivation {
+          pname = "gzstat";
+          version = "1.0";
+          src = pkgs.fetchFromGitHub {
+            owner = "billbird";
+            repo = "gzstat";
+            rev = "fd6aeaeacf26ad13e22150038600c3461af35b94";
+            sha256 = "sha256-DcisxhZH3H0sn7y/BPg2k6eu3aGmJ13u3bmpHyIOSKc=";
+          };
+          dontBuild = true;
+          installPhase = ''
+            mkdir -p $out/bin
+            echo '#!${pkgs.python313}/bin/python' > $out/bin/gzstat
+            cat $src/gzstat.py >> $out/bin/gzstat
+            chmod +x $out/bin/gzstat
+          '';
+        };
+      in
+      {
+        devShells.default = pkgs.mkShell {
+          packages = with pkgs; [
+            cmake
+            catch2_3
+            python313
+            llvmPackages_20.clang-tools
+            lcov
+            flamegraph.packages.${system}.default
+            gzstat
+          ];
+        };
+      }
+    );
+}
