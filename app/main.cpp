41b7a471117ee8e8a254e482f0ce58fb9de51ed6
Callum Curtis <105407637+callumcurtis@users.noreply.github.com>
2025-10-14 16:47:58 -0700

    refactor: main.cpp

--- app/main.cpp
+++ app/main.cpp
@@ -6,7 +6,6 @@
 #include <limits>
 #include <memory>
 
-// https://github.com/d-bahr/CRCpp
 #define CRCPP_USE_CPP11
 #include "third_party/CRC.h"
 
@@ -18,133 +17,131 @@
 #include "gz.hpp"
 #include "lzss.hpp"
 
-struct BlockStreamWithBreakpoint {
+struct BlockStreamWithMaximumBlockSize {
   std::unique_ptr<BlockStream> block_stream;
-  std::size_t breakpoint; // The maximum number of uncompressed bytes that can
-                          // be stored in a block in the stream
+  std::size_t maximum_uncompressed_bytes_in_block;
 };
 
 auto main() -> int {
-  // Untie stdout from stdout
+  // Untie stdin from stdout to avoid flushing output before each read
   std::cin.tie(nullptr);
 
-  gz::BitStream stream{std::cout};
-
   auto crc_table = CRC::CRC_32().MakeTable();
+  std::uint32_t crc{};
 
+  gz::BitStream stream{std::cout};
   stream.push_header();
-
-  std::uint32_t bytes_read{0};
-  std::uint32_t uncompressed_block_size{0};
-
-  char next_byte{}; // Note that we have to use a (signed) char here for
-                    // compatibility with istream::get()
 
   // We need to see ahead of the stream by one character (e.g. to know, once we
   // fill up a block, whether there are more blocks coming), so at each step,
   // next_byte will be the next byte from the stream that is NOT in a block.
+  char next_byte{};
 
-  std::uint32_t crc{};
+  std::uint32_t num_uncompressed_bytes_in_file{0};
+  std::uint32_t num_uncompressed_bytes_in_block{0};
 
-  const auto block_streams_with_breakpoints =
-      std::array<BlockStreamWithBreakpoint, 3>{
-          BlockStreamWithBreakpoint{
+  const auto block_streams_with_maximum_block_sizes =
+      std::array<BlockStreamWithMaximumBlockSize, 3>{
+          BlockStreamWithMaximumBlockSize{
               .block_stream = std::make_unique<
                   BlockType0Stream<maximum_block_type_0_capacity>>(stream),
-              .breakpoint = maximum_block_type_0_capacity},
+              .maximum_uncompressed_bytes_in_block =
+                  maximum_block_type_0_capacity},
           // Block type 1 is only suitable for small blocks, where the overhead
           // of block type 2 is comparatively large. Since the warmup period for
           // the change point detector is larger than the maximum desirable size
           // of a block of type 1, disable block type 1 entirely by setting its
           // breakpoint to 0. This improves speed by reducing the number of LZSS
           // searches.
-          BlockStreamWithBreakpoint{
+          BlockStreamWithMaximumBlockSize{
               .block_stream = std::make_unique<BlockType1Stream<
                   maximum_look_back_size, maximum_look_ahead_size>>(stream),
-              .breakpoint = 0},
-          BlockStreamWithBreakpoint{
+              .maximum_uncompressed_bytes_in_block = 0},
+          BlockStreamWithMaximumBlockSize{
               .block_stream = std::make_unique<BlockType2Stream<
                   maximum_look_back_size, maximum_look_ahead_size>>(stream),
-              .breakpoint = 1U << 30U}};
+              .maximum_uncompressed_bytes_in_block = 1U << 30U}};
+
   CusumDistributionDetector change_point_detector(
       {.t_warmup = 1U << 13U, // NOLINT (cppcoreguidelines-avoid-magic-numbers)
        .h_threshold = 1e3});  // NOLINT (cppcoreguidelines-avoid-magic-numbers)
-  const auto last_breakpoint =
-      std::ranges::max_element(block_streams_with_breakpoints,
-                               [](const auto &a, const auto &b) {
-                                 return a.breakpoint < b.breakpoint;
-                               })
-          ->breakpoint;
 
-  auto commit_smallest = [&block_streams_with_breakpoints,
-                          &uncompressed_block_size](bool is_last) {
+  const auto maximum_of_maximum_uncompressed_block_sizes =
+      std::ranges::max_element(block_streams_with_maximum_block_sizes,
+                               [](const auto &a, const auto &b) {
+                                 return a.maximum_uncompressed_bytes_in_block <
+                                        b.maximum_uncompressed_bytes_in_block;
+                               })
+          ->maximum_uncompressed_bytes_in_block;
+
+  auto commit_smallest = [&block_streams_with_maximum_block_sizes,
+                          &num_uncompressed_bytes_in_block](bool is_last) {
     std::uint8_t smallest_compressed_block_type =
         std::numeric_limits<std::uint8_t>::max();
     std::size_t smallest_compressed_block_size =
         std::numeric_limits<std::size_t>::max();
-    for (auto i = 0; i < block_streams_with_breakpoints.size(); ++i) {
-      if (uncompressed_block_size >
-          block_streams_with_breakpoints.at(i).breakpoint) {
+    for (auto i = 0; i < block_streams_with_maximum_block_sizes.size(); ++i) {
+      if (num_uncompressed_bytes_in_block >
+          block_streams_with_maximum_block_sizes.at(i)
+              .maximum_uncompressed_bytes_in_block) {
         continue;
       }
       const auto compressed_block_size =
-          block_streams_with_breakpoints.at(i).block_stream->bits(is_last);
+          block_streams_with_maximum_block_sizes.at(i).block_stream->bits(
+              is_last);
       if (compressed_block_size < smallest_compressed_block_size) {
         smallest_compressed_block_size = compressed_block_size;
         smallest_compressed_block_type = static_cast<std::uint8_t>(i);
       }
     }
-    block_streams_with_breakpoints.at(smallest_compressed_block_type)
+    block_streams_with_maximum_block_sizes.at(smallest_compressed_block_type)
         .block_stream->commit(is_last);
   };
 
   if (std::cin.get(next_byte)) {
-    bytes_read++;
-    uncompressed_block_size++;
-    // Update the CRC as we read each byte (there are faster ways to do this)
-    crc = CRC::Calculate(&next_byte, 1,
-                         crc_table); // This call creates the initial CRC value
-                                     // from the first byte read.
-    // Read through the input
+    num_uncompressed_bytes_in_file++;
+    num_uncompressed_bytes_in_block++;
+    crc = CRC::Calculate(&next_byte, 1, crc_table);
     while (true) {
-      for (const auto &block_stream_with_breakpoint :
-           block_streams_with_breakpoints) {
-        if (uncompressed_block_size >=
-            block_stream_with_breakpoint.breakpoint) {
+      for (const auto &block_stream_with_maximum_block_size :
+           block_streams_with_maximum_block_sizes) {
+        if (num_uncompressed_bytes_in_block >=
+            block_stream_with_maximum_block_size
+                .maximum_uncompressed_bytes_in_block) {
           continue;
         }
-        block_stream_with_breakpoint.block_stream->put(next_byte);
+        block_stream_with_maximum_block_size.block_stream->put(next_byte);
       }
       const auto is_change_point_detected =
           change_point_detector.step(next_byte).second;
       if (!std::cin.get(next_byte)) {
         break;
       }
-      bytes_read++;
-      uncompressed_block_size++;
-      crc = CRC::Calculate(&next_byte, 1, crc_table,
-                           crc); // Add the character we just read to the CRC
-                                 // (even though it is not in a block yet)
+      num_uncompressed_bytes_in_file++;
+      num_uncompressed_bytes_in_block++;
+      crc = CRC::Calculate(&next_byte, 1, crc_table, crc);
 
       if (is_change_point_detected ||
-          uncompressed_block_size >= last_breakpoint) {
+          num_uncompressed_bytes_in_block >=
+              maximum_of_maximum_uncompressed_block_sizes) {
         commit_smallest(false);
-        for (const auto &block_stream_with_breakpoint :
-             block_streams_with_breakpoints) {
-          block_stream_with_breakpoint.block_stream->reset();
+        for (const auto &block_stream_with_maximum_block_size :
+             block_streams_with_maximum_block_sizes) {
+          block_stream_with_maximum_block_size.block_stream->reset();
         }
         change_point_detector.reset();
-        uncompressed_block_size = 0;
+        num_uncompressed_bytes_in_block = 0;
       }
     }
 
     commit_smallest(true);
 
-    // Pad to byte boundary before returning to gz
+    // Pad to byte boundary before returning from deflate bitstream to gz
+    // bitstream
     stream.flush_byte();
   }
 
-  stream.push_footer(crc, bytes_read);
+  stream.push_footer(crc, num_uncompressed_bytes_in_file);
 
   return 0;
 }
