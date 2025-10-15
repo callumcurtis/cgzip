1badb559b21ef2597baa4620d598f4b14bbefb6b
Callum Curtis <105407637+callumcurtis@users.noreply.github.com>
2025-10-15 12:57:37 -0700

    refactor: constants

--- Makefile
+++ Makefile
@@ -50,6 +50,7 @@ fix: lint format
 verify:
 	find app include src test -type f \( -name "*.cpp" -o -name "*.hpp" \) | xargs --no-run-if-empty clang-format --dry-run --Werror
 	git diff --name-only --diff-filter=d HEAD | grep -E 'include/.*\.hpp$$|src/.*\.cpp$$|src/.*\.hpp$$|app/.*\.cpp$$|app/.*\.hpp$$' | xargs --no-run-if-empty -n 1 -P 8 clang-tidy -p $(build-dir)
+	make clean && make test
 
 .PHONY: setup-pre-commit
 setup-pre-commit:
--- include/constants.hpp
+++ include/constants.hpp
@@ -2,12 +2,12 @@
 
 #include <cstdint>
 
-const std::uint16_t num_literal_length_symbols = 288;
-const std::uint16_t num_distance_symbols = 30;
-const std::uint16_t num_length_symbols = 29;
-const std::uint16_t eob_symbol = 256;
-const std::uint16_t minimum_back_reference_length = 3;
-const std::uint16_t minimum_back_reference_distance = 1;
-const std::uint16_t maximum_look_back_size = 1U << 15U;
-const std::uint16_t maximum_look_ahead_size = 258;
-const std::uint8_t maximum_prefix_code_length = 15;
+constexpr std::uint16_t num_literal_length_symbols = 288;
+constexpr std::uint16_t num_distance_symbols = 30;
+constexpr std::uint16_t num_length_symbols = 29;
+constexpr std::uint16_t eob_symbol = 256;
+constexpr std::uint16_t minimum_back_reference_length = 3;
+constexpr std::uint16_t minimum_back_reference_distance = 1;
+constexpr std::uint16_t maximum_look_back_size = 1U << 15U;
+constexpr std::uint16_t maximum_look_ahead_size = 258;
+constexpr std::uint8_t maximum_prefix_code_length = 15;
